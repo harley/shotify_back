50b6699466936b136ca6746b3fc9474c8b5878c7
Harley Trung <harley@socialsci.com>
2012-10-19 22:26:35 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShotifyBackbend::Application.config.secret_token = 'ba6d92f7905486db41180c90ab540f1942c075ec783f8e8c1e492a61a7413c865aa5a14b763192e2c52629c60126ca00d6303a1fea139422a5856f2d123d8ba0'
